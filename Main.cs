9988fe16e91e99e47429b6c0001d3651654b4bfc
kdp <torinoyume@naver.com>
2019-12-29 18:15:28 +0900

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+  int fps;
+  string imageType;
+  bool hasVoice;
+  Vector3 webGLVector;
+  
+  void Start() {
+  }
+  
+  void Update() {
+  }
+  
+}
